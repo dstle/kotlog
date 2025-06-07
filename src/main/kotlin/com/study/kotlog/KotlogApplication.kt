bdb14ba1ef0c616c8d6bd3424441558b34365f29
sudo-dustle <asdf748596123@gmail.com>
2025-06-07 21:32:16 +0900

    feat: first commit

---
+++ src/main/kotlin/com/study/kotlog/KotlogApplication.kt
@@ -0,0 +1,11 @@
+package com.study.kotlog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlogApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/kotlog/KotlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.kotlog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
