14f3cb4aa042f239b127f16a8af415eb97324b7d
dustle <pwdustle@gmail.com>
2025-06-11 22:35:05 +0900

    refactor: 엔티티에서 id/createdAt/updatedAt 추출하여 BaseEntity로 분리

---
+++ src/main/kotlin/com/study/kotlog/domain/BaseEntity.kt
@@ -0,0 +1,25 @@
+package com.study.kotlog.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.MappedSuperclass
+import java.time.ZonedDateTime
+
+@MappedSuperclass
+abstract class BaseEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val _id: Long? = null
+    val id: Long
+        get() = _id!!
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: ZonedDateTime = ZonedDateTime.now()
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: ZonedDateTime = ZonedDateTime.now()
+}
---
+++ src/main/kotlin/com/study/kotlog/domain/post/Post.kt
@@ -0,0 +1,20 @@
+package com.study.kotlog.domain.post
+
+import com.study.kotlog.domain.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "posts")
+class Post(
+
+    @Column(name = "post_id", nullable = false)
+    val postId: Long,
+
+    @Column(name = "author_id", nullable = false)
+    val authorId: Long,
+
+    @Column(name = "content", nullable = false)
+    val content: String
+) : BaseEntity()
