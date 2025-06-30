443f5f43865b901e52ff9a9a147a9b112b6c89a7
dustle <pwdustle@gmail.com>
2025-06-30 23:14:41 +0900

    feat: swagger 적용

--- src/main/kotlin/com/study/kotlog/front/controller/UserController.kt
+++ src/main/kotlin/com/study/kotlog/front/controller/UserController.kt
@@ -2,6 +2,7 @@ package com.study.kotlog.front.controller
 
 import com.study.kotlog.domain.user.UserRepository
 import com.study.kotlog.front.controller.dto.UserResponse
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -14,8 +15,11 @@ class UserController(
 ) {
 
     @GetMapping("/me")
+    @Operation(summary = "내 정보 조회", description = "userId 를 기반으로 사용자 정보를 조회합니다.")
     fun me(userId: Long): ResponseEntity<UserResponse> {
-        val user = userRepository.findById(userId).get()
+        val user = userRepository.findById(userId)
+            .orElseThrow { IllegalArgumentException("유저를 찾을 수 없습니다. id=$userId") }
+
         return ResponseEntity.ok(UserResponse.from(user))
     }
 }
